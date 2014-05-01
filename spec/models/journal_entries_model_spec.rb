4637d004b34b38324656b22e8c523cc4681286b2
Christoph Wong <wong.christoph@gmail.com>
2014-04-30 21:01:05 -0500

    tested model for journal

--- spec/models/journal_entries_model_spec.rb
+++ spec/models/journal_entries_model_spec.rb
@@ -6,10 +6,13 @@ describe JournalEntry do
   end
 
   it { should belong_to(:user) }
-  it { should have_many(:tags) }
+  it { should have_many(:tags), :as => :taggable }
+  it { should validate_presence_of(:content) }
+  it { should validate_presence_of(:emotion_rating) }
 
   it "is invalid without a lastname" do
     FactoryGirl.build(:journal_entry, content: nil).should_not be_valid
   end
 
 end
+
