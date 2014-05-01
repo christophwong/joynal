d2ba5cdb9579f0515e3c79ff72f42c8362a2bd5f
Harry Wang <hgw2101@gmail.com>
2014-05-01 11:07:42 -0500

    experiment with d3

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  // d3.selectAll('h1').style("color", function() {
+  //   return "hsl("+ Math.random() * 360 +", 100%, 50%)";
+  // });
+
+  // d3.selectAll('h2')
+  //   .data([19, 70, 12, 16])
+  //   .style('font-size', function(d) {
+  //     return d + 'px'
+  //   })
+  //   .enter().append('h2')
+  //   .text(function(d) {
+  //     return "I'm h2 number" + d +"!";
+  //   });
+
+  // var String = "Fucktarts"
+
+  // var h2 = d3.select("body").selectAll("p")
+  //   .data([4, 8, 15, 16, 23, 42])
+  //   .text(String);
+
+  // entering
+  // h2.enter().append('h2').text(String);
+
+
+  // exiting
+  // h2.exit().remove();
+
+
+  // d3.select('body')
+  //   .transition()
+  //   .duration(750)
+  //   .delay(1000)
+  //   .style('background-color', 'black');
+
+  var svg = d3.select('body').selectAll('svg')
+  var circle = svg.selectAll("circle");
+
+  $('button').on('click', function(e) {
+    circle.transition().duration(500).delay(0).style('fill', 'black');
+    circle.transition().duration(750).delay(500).attr('r',30);
+    circle.transition().duration(1200).delay(1250).attr('cy', 120);
+  })
+
+
+});
