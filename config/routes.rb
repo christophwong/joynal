fb2ff0afd8d5503018fd1912c26b2bfa1dba8b1c
Harry Wang <hgw2101@gmail.com>
2014-04-30 19:26:50 -0500

    make tags work for journal entries and be able to view a list of journals for a specific tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'journal_entries#index'
+  get 'journal_entries/tags/:name', to: 'journal_entries#show_tagged', as: 'tags'
   resources :journal_entries
+
+  resources :acts_as_taggable_ons
+
 end
