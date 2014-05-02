b6fcb098274bafc2e418554996519417f5d619d6
Greg Heaphy <heaphyg@gmail.com>
2014-05-01 19:05:47 -0500

    a few but not all tests for journal entry controller have been completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def stored_location_for(resource)
-    nil
-  end
+  # def stored_location_for(resource)
+  #   nil
+  # end
 
   def after_sign_in_path_for(resource)
     journal_entries_path
   end
+  
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'journal_entries/tags/:name', to: 'journal_entries#show_tagged', as: 'tag'
-  resources :journal_entries
+  resources :journal_entries, :only => [:index, :new, :create, :show]
 
   devise_for :users
   root 'static#index'
