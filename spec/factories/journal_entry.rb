fedbf5372eab43efaef6e7cd009c14584c770e62
Greg Heaphy <heaphyg@gmail.com>
2014-05-01 22:10:20 -0500

    one test left for the journal entry controller

--- spec/factories/journal_entry.rb
+++ spec/factories/journal_entry.rb
@@ -7,3 +7,12 @@ FactoryGirl.define do
     f.emotion_rating { rand(1..5)}
   end
 end
+
+
+FactoryGirl.define do
+	factory :invalid_journal_entry, parent: :journal_entry do |f|
+       f.user_id { rand(1..5)}
+    f.content nil
+    f.emotion_rating { rand(1..5)}
+	end
+end
