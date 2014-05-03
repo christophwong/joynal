e1c7a77267658c5b06207bb6fa6cac480bac41ce
Jake Nations <jnations1214@gmail.com>
2014-05-02 19:12:03 -0500

    devise keys

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
 
   get 'journal_entries/tags/:name', to: 'journal_entries#show_tagged', as: 'tag'
-  post 'journal_entries/:id/show_tagged', to: 'journal_entries#show_graph', as: 'show_graph'
   resources :journal_entries, :only => [:index, :new, :create, :show]
 
-  # this registration route is no longer necessary
-  # now that we are using the default Devise controller
-  devise_for :users #, :controllers => {:registrations => "registrations"}
+  devise_for :users
   root 'static#index'
   authenticate :user do
     mount Sidekiq::Web, at: "/sidekiq"
