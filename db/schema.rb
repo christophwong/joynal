bb714540e8ce27d6d0828991931e6b7219ab3371
Harry Wang <hgw2101@gmail.com>
2014-05-03 21:22:12 -0500

    added index to keywords.sentiment_score and journal_entries.created_at to speed up query for wordcloud

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140503200026) do
+ActiveRecord::Schema.define(version: 20140504021955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140503200026) do
     t.string   "sentiment_type"
   end
 
+  add_index "journal_entries", ["created_at"], name: "index_journal_entries_on_created_at", using: :btree
   add_index "journal_entries", ["user_id"], name: "index_journal_entries_on_user_id", using: :btree
 
   create_table "keywords", force: true do |t|
@@ -38,6 +39,7 @@ ActiveRecord::Schema.define(version: 20140503200026) do
 
   add_index "keywords", ["journal_entry_id"], name: "index_keywords_on_journal_entry_id", using: :btree
   add_index "keywords", ["name"], name: "index_keywords_on_name", using: :btree
+  add_index "keywords", ["sentiment_score"], name: "index_keywords_on_sentiment_score", using: :btree
 
   create_table "quotes", force: true do |t|
     t.text   "body"
