894cf5e0f7f2e165af8a4c35d556cdba600ad205
Jake Nations <jnations1214@gmail.com>
2014-05-03 22:38:37 -0500

    journal_entry#index all functions are now ajaxed. ajaxed all the things:

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'journal_entries/tags/:name', to: 'journal_entries#show_tagged', as: 'tag'
   post 'journal_entries/:id/show_graph', to: 'journal_entries#show_graph', as: 'show_graph'
+  get "journal_entries/list", to: "journal_entries#list"
+  get "journal_entries/entry", to: "journal_entries#entry"
   resources :journal_entries, :only => [:index, :new, :create, :show]
 
   devise_for :users, :controllers => {:registrations => "registrations", :sessions => "sessions"}
