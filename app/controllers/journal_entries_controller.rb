de89f3e1865d7427cf09f2ec44213adec97e9a22
Harry Wang <hgw2101@gmail.com>
2014-05-05 09:48:38 -0500

    change the journal_entries#get_line_chart so that it orderes the data based on created_at desc

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -133,7 +133,7 @@ class JournalEntriesController < ApplicationController
   end
 
   def get_line_chart
-    @journal_entries = current_user.journal_entries
+    @journal_entries = current_user.journal_entries.order('created_at DESC')
     respond_to do |format|
       format.json { render json: @journal_entries }
     end
