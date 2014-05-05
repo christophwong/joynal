63c7b083b3e947245b9387929ee475107e3ea393
Jake Nations <jnations1214@gmail.com>
2014-05-05 16:56:13 -0500

    come on unicorn dont fail me now

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -20,7 +20,6 @@ after_fork do |server, worker|
 
   if defined?(ActiveRecord::Base)
     config = Rails.application.config.database_configuration[Rails.env]
-    config['adapter'] = 'postgis'
     ActiveRecord::Base.establish_connection(config)
   end
 end
