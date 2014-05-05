0e18b1cdc066ad9a20d4492c6b23850993113b03
Jake Nations <jnations1214@gmail.com>
2014-05-05 16:52:22 -0500

    come on

--- config/application.rb
+++ config/application.rb
@@ -1,26 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-class ActiveRecordOverrideRailtie < Rails::Railtie
-  initializer "active_record.initialize_database.override" do |app|
 
-    ActiveSupport.on_load(:active_record) do
-      if url = ENV['DATABASE_URL']
-        ActiveRecord::Base.connection_pool.disconnect!
-        parsed_url = URI.parse(url)
-        config =  {
-          adapter:             'postgis',
-          host:                parsed_url.host,
-          encoding:            'unicode',
-          database:            parsed_url.path.split("/")[-1],
-          port:                parsed_url.port,
-          username:            parsed_url.user,
-          password:            parsed_url.password
-        }
-        establish_connection(config)
-      end
-    end
-  end
-end
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_record/railtie"
@@ -44,7 +24,7 @@ module Ponies
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+     config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
