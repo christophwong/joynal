64b54657e8edea2271d32644c0f019085628b6fd
Jake Nations <jnations1214@gmail.com>
2014-05-06 09:02:38 -0500

    precompiled assets and updated quote seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,30 @@
-# require 'unirest'
+require 'unirest'
 
-# counter = 1
+counter = 1
 
-# 5.times do 
-#   puts "importing quote number #{counter}"
-#   response = Unirest::get "https://theysaidso.p.mashape.com/quote?category=motivational", 
-#     headers: { 
-#       "X-Mashape-Authorization" => ENV["QUOTE_KEY"],
-#       "Accept" => "application/json"
-#     }
+50.times do 
+  puts "importing quote number #{counter}"
+  response = Unirest::get "https://theysaidso.p.mashape.com/quote?category=motivational", 
+    headers: { 
+      "X-Mashape-Authorization" => ENV["QUOTE_KEY"],
+      "Accept" => "application/json"
+    }
 
-#   Quote.create(body: response.body["contents"]["quote"],
-#               author: response.body["contents"]["author"])
-#   counter += 1
-# end
-
-# 5.times do 
-#   puts "importing quote number #{counter}"
-#   response = Unirest::get "https://theysaidso.p.mashape.com/quote?category=inspirational", 
-#     headers: { 
-#       "X-Mashape-Authorization" => ENV["QUOTE_KEY"],
-#       "Accept" => "application/json"
-#     }
-
-#   Quote.create(body: response.body["contents"]["quote"],
-#               author: response.body["contents"]["author"])
-#   counter += 1
-# end
-
-500.times do 
-  lat_lon = "POINT (#{Faker::Address.latitude} #{Faker::Address.longitude})"
-  point = LocationRecord::GEOFACTORY.parse_wkt(lat_lon)
-  LocationRecord.create(coords: point.projection) if point
+  Quote.create(body: response.body["contents"]["quote"],
+              author: response.body["contents"]["author"])
+  counter += 1
 end
+
+50.times do 
+  puts "importing quote number #{counter}"
+  response = Unirest::get "https://theysaidso.p.mashape.com/quote?category=inspirational", 
+    headers: { 
+      "X-Mashape-Authorization" => ENV["QUOTE_KEY"],
+      "Accept" => "application/json"
+    }
+
+  Quote.create(body: response.body["contents"]["quote"],
+              author: response.body["contents"]["author"])
+  counter += 1
+end
+
