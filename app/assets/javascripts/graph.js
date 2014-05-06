1cda1c1611b91223e68f471f1f019dab9dbe35e5
Greg Heaphy <heaphyg@gmail.com>
2014-05-06 09:39:57 -0500

    now the the center d3 number is more centered

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -39,7 +39,7 @@ function showGraph() {
 
 
     var displayText = svg.append('text')
-       .attr('transform', "translate(0, 0)")
+       .attr('transform', "translate(-10, 9)")
        .attr('font-size', '2em')
 
     g.append("path")
