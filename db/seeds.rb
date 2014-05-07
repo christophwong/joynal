ea970c74f5e6aca9f3cc35acb0f631a4e641b12a
Harry Wang <hgw2101@gmail.com>
2014-05-07 13:41:53 -0500

    add twitter API methods in the seed file and add the twitter gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,14 +58,30 @@ end
 
 # assign random sentiment_score
 
-100.times do
-  entry = JournalEntry.new(
-    content: Faker::Lorem.paragraph(5),
-    emotion_rating: rand(1..5),
-    user: User.find(rand(User.first.id..User.last.id))
-    )
-  entry.sentiment_score = rand(-10..-5)/10.0
-  lat_lon = "POINT (#{41.8613538 + rand(-10..10)/1000.0} #{-87.6929968+rand(-10..10)/1000.0})"
-  save_location(lat_lon, entry)
+# 100.times do
+#   entry = JournalEntry.new(
+#     content: Faker::Lorem.paragraph(5),
+#     emotion_rating: rand(1..5),
+#     user: User.find(rand(User.first.id..User.last.id))
+#     )
+#   entry.sentiment_score = rand(-10..-5)/10.0
+#   lat_lon = "POINT (#{41.8613538 + rand(-10..10)/1000.0} #{-87.6929968+rand(-10..10)/1000.0})"
+#   save_location(lat_lon, entry)
+# end
+
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "NuS3bc0z3xx5w66Tn9zgyd2uI"
+  config.consumer_secret    = ENV['CONSUMER_SECRET']
+  config.access_token        = "33998273-iX45VdrCQrfSwQ2f2EoMbegS4b1AeDlai1BNpxZ4H"
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
 
+bourdain = User.create(first_name: "Anthony", last_name: "Bourdain", email: 'anthony@http://bourdain.com', password: "password", password_confirmation: "password" )
+
+client.user_timeline("Bourdain", {count: 15}).each do |tweet|
+  emotion_rating = rand(1..5)
+  lat_lon = "POINT (#{Faker::Address.latitude} #{Faker::Address.longitude})"
+  j = JournalEntry.create(user_id: http://bourdain.id ,content: tweet.text, emotion_rating: emotion_rating)
+  LocationRecord.create(journal_entry: j, location: lat_lon)
+end
